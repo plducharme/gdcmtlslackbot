c9db0ae96ae13ecc83d7217d0f27a4de096ad5b7
Pier Luc Ducharme <dutch80@gmail.com>
2018-11-05 19:45:01 -0500

    Added basic firebase support - temporary commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,70 @@
 import * as functions from 'firebase-functions';
 import {WebhookClient} from 'dialogflow-fulfillment';
-
-
+import * as admin from 'firebase-admin';
+import {DocumentData} from '@google-cloud/firestore';
 
 'use strict';
 
 process.env.DEBUG = 'dialogflow:debug';
 
+
+const firebaseApp = admin.initializeApp(functions.config().firebase); 
+const db = firebaseApp.firestore();
+
+
+
+class OpenTable {
+    eventDate : Date;
+    subject : string;
+
+}
+
+const openTables : Map<string, OpenTable> = new Map<string, OpenTable>();  
+
+const getNextOpenTables = function () : OpenTable {
+    let nextOpenTable = null;
+    
+    openTables.forEach(openTable => {
+        if(nextOpenTable == null) {
+            nextOpenTable = openTable;
+        } else {
+            //if()
+        }
+
+    });
+
+    return nextOpenTable;
+}  
+
+const convertDataToOpenTable  = function (data : DocumentData, id : string) : OpenTable{
+    const opentable : OpenTable = new OpenTable();
+    opentable.eventDate = data.eventDate;
+    opentable.subject = data.subject;
+    
+    return opentable;
+}   
+
+
+
+
+const getAllScheduledOpenTables = () => {
+    db.collection('opentables').get().then((snapshot) => {
+        snapshot.forEach((doc) => {
+            const data = doc.data;
+            openTables.set(doc.id, convertDataToOpenTable(doc.data, doc.id));
+        });    
+    }).catch((error) => {
+        console.log("Error while fetching OpenTables from db " + error);
+    });
+
+
+
+}
+
+
+
 export const dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
     const webhookAgent = new WebhookClient({ request, response });
-    
 
     function welcome(agent) {
         agent.add("Bonjour!");
@@ -23,7 +78,9 @@ export const dialogflowFirebaseFulfillment = functions.https.onRequest((request,
 
     function dateProchainOpenTables(agent) {
         console.log("dateProchainOpenTables called!");
-        agent.add("Laissez moi regarder ce que j'ai écrit sur mon calendrier scout");
+        agent.add("Laissez moi regarder ce que j'ai écrit sur mon calendrier scout!");
+
+        
     }
 
 
@@ -31,6 +88,8 @@ export const dialogflowFirebaseFulfillment = functions.https.onRequest((request,
     intentMap.set('Default Welcome Intent', welcome);
     intentMap.set('Default Fallback Intent', fallback);
     intentMap.set('DateProchainOpenTables', dateProchainOpenTables);
+
+    getAllScheduledOpenTables();
     
     webhookAgent.handleRequest(intentMap);
 
